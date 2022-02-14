c302815ff75451002922d9bcc43f64532f184c8c
Martin Hübner <martin.hubner@web.de>
2022-02-14 13:57:46 +0100

    stuff

--- packages/falter-berlin-service-registrar/files/prepare_site.sh
+++ packages/falter-berlin-service-registrar/files/prepare_site.sh
@@ -2,14 +2,14 @@
 
 . /lib/config/uci.sh
 
-SITE="freifunk-ag"
-DESCRIPTION="Freifunk-AG TUB"
+SITE="testsite"
+DESCRIPTION="Testseite zum Testen"
 PORT="81"
 WWW_PATH="/tmp/www/"
 PROTO="tcp"
 
 # load site from source
-mkdir /tmp/www/${SITE}
+mkdir -p /tmp/www/${SITE}
 
 # configure site in uhttpd
 uci_add uhttpd uhttpd ${SITE}
@@ -18,6 +18,7 @@ uci_add_list uhttpd ${SITE} listen_http "[::]:${PORT}"
 uci_set uhttpd ${SITE} home "${WWW_PATH}${SITE}/"
 uci_set uhttpd ${SITE} max_requests 5
 uci_set uhttpd ${SITE} max_connections 100
+uci commit uhttpd
 
 # announce service via olsrd nameservice plugin
 #list hosts '10.31.142.65 freifunk-ag'
@@ -34,10 +35,17 @@ uci_set uhttpd ${SITE} max_connections 100
 #olsrd.@LoadPlugin[2].service='http://freifunk-ag.olsr:81|tcp|Freifunk-AG TUB'
 
 
-SERVICE_ADDR="10.31.142.65" # first IP-addr of dhcp-subnet
+SERVICE_ADDR=$(uci_get network dhcp ipaddr) # first IP-addr of dhcp-subnet
 
 HOSTLINE="${SERVICE_ADDR} ${SITE}"
 SERVICE_LINE="http://${SITE}.olsr:${PORT}|${PROTO}|${DESCRIPTION}"
 
-uci_add_list olsrd LoadPlugin[2] hosts "$HOSTLINE"
-uci_add_list olsrd LoadPlugin[2] service "$SERVICE_LINE"
+# get number of nameservice_plugin in olsrd-config
+NS_PLUGIN=$(uci show olsrd | grep nameservice | sed -e 's|.*\(\d\).*|\1|g')
+
+uci_add_list olsrd @LoadPlugin["$NS_PLUGIN"] hosts "$HOSTLINE"
+uci_add_list olsrd @LoadPlugin["$NS_PLUGIN"] service "$SERVICE_LINE"
+uci_commit olsrd
+
+/etc/init.d/uhttpd restart
+/etc/init.d/olsrd restart
